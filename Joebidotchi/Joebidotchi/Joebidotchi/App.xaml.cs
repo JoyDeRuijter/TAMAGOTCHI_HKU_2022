2a04bf597aa33c299a7028acee3366bff8b461c8
Joy <joyderuijter@gmail.com>
2022-09-15 16:45:14 +0200

    Create a whole new project

---
+++ Joebidotchi/Joebidotchi/Joebidotchi/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Joebidotchi.Services;
+using Joebidotchi.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Joebidotchi
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Joebidotchi/Joebidotchi/Joebidotchi/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Joebidotchi.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Joebidotchi.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
